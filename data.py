1fd91883c20786365802c24e6a5ac4ccf5770ef6
MichelleC97 <MIchellec401@nycstudents.net>
2025-02-13 10:13:39 -0500

    commit

--- data.py
+++ data.py
@@ -1,10 +1,5 @@
-x = "test"
-print(f"hello {x}")
-
-temp = 78
-if temp > 68:
-    print('warm')
-elif temp == 68:
-    print('perfect')
-else:
-    print('cold')
+number = input("Is it even?")
+if number == "even":
+    print('even')
+elif number == "odd":
+    print('odd')
