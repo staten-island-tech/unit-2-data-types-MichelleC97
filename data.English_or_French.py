61b510dfbf0b68c86958714a6c1d3f3fe79656ca
MichelleC97 <MIchellec401@nycstudents.net>
2025-02-26 10:14:44 -0500

    commit

---
+++ data.English_or_French.py
@@ -0,0 +1,25 @@
+""" text = input("The red cat sat on the mat. Why are you so sad cat? Don't ask that.")
+def input(S,s):
+    def input(T,t):
+        if text == (S,s < T,t):
+            print('English')
+        elif text == (S,s > T,t):
+            print('French')
+        elif text == (S,s = T,t):
+            print('French') """
+
+
+
+
+language = ("text")
+def input(English):
+    def input(French):
+        def input(S,T):
+            def input(s,t):
+                for text in range(S<T):
+                    print(English)
+                for text in range(S>T):
+                    print(French)
+                for text in range(S=T):
+                    print(French)
+
